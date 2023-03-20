55e2fd41ab176a61da688daa323cd2e21cb9bd75
Eric <mugoericpg20@gmail.com>
2023-03-20 19:53:03 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solutions in this file
+const myArray = [];
+
+function writeCards(myArray = ["Guadalupe", "Ollie", "Aki"] , event = "surprise"){
+  for(let i = 0 ; i<myArray.length; i++){
+
+    console.log(`Thank you, ${myArray[i]}, for the wonderful ${event} gift!`)
+
+
+  }
+  return myArray;
+}
+
+function countDown(n){
+  while(n>=0){
+
+   console.log(n);
+
+    n--;
+
+  }
+
+
+}
